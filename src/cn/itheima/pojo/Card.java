db4a765ed4d129f839348d02788bcd95bbe157f3
oweson <570347720@qq.com>
2018-08-24 14:39:05 +0800

    我的提交

---
+++ src/cn/itheima/pojo/Card.java
@@ -0,0 +1,23 @@
+package cn.itheima.pojo;
+
+public class Card {
+    private Integer id;
+
+    private String code;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+}
